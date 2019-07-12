62fba583c164c98034c92ac54c2524d91d0d7498
Drod2099 <37273083+Drod2099@users.noreply.github.com>
2019-07-12 16:47:35 -0400

    Added options screen functionality. Needs difficulty to change and color inversion to work

--- main.py
+++ main.py
@@ -17,13 +17,20 @@ options_font = pygame.font.SysFont("Bauhaus 93", 72)
 clock = pygame.time.Clock()
 
 done = False
-start_screen = True
+start_screen = False
 round_start = False
 win_screen = False
 lose_screen = False
-options_screen = False
+options_screen = True
 
 start_direction = "right"
+
+WHT = (255, 255, 255)
+GREY = (100, 100, 100)
+
+dif = "Medium"
+max_score = 11
+color_inversion = "Off"
 
 round_count = 0
 enemy_points = 0
@@ -40,6 +47,16 @@ exit_box = pygame.Rect(311, 521, 141, 69)
 
 restart_box = pygame.Rect(305, 380, 180, 65)
 main_menu_box = pygame.Rect(260, 465, 280, 65)
+
+dif_easy_box = pygame.Rect(295, 155, 105, 55)
+dif_medium_box = pygame.Rect(445, 155, 180, 55)
+dif_hard_box = pygame.Rect(670, 155, 110, 55)
+color_inv_on_box = pygame.Rect(395, 415, 75, 50)
+color_inv_off_box = pygame.Rect(545, 415, 85, 50)
+eleven_score_box = pygame.Rect(495, 285, 65, 50)
+seven_score_box = pygame.Rect(395, 285, 40, 50)
+fifteen_score_box = pygame.Rect(620, 285, 65, 50)
+back_box = pygame.Rect(345, 525, 120, 50)
 
 player = PlayerPad()
 enemy = EnemyPad()
@@ -66,11 +83,11 @@ while not done:
             puck.update(deltaTime)
 
         # Win-Lose Check
-        if enemy_points == 11:
+        if enemy_points == max_score:
             lose_screen = True
             enemy_points = 0
             player_points = 0
-        elif player_points == 11:
+        elif player_points == max_score:
             win_screen = True
             enemy_points = 0
             player_points = 0
@@ -126,26 +143,27 @@ while not done:
     elif evt.type == pygame.MOUSEBUTTONDOWN and evt.button == 1 and options_screen:
         mpos = pygame.mouse.get_pos()
         if dif_easy_box.collidepoint(mpos):
-            pass
+            dif = "Easy"
         if dif_medium_box.collidepoint(mpos):
-            pass
+            dif = "Medium"
         if dif_hard_box.collidepoint(mpos):
-            pass
+            dif = "Hard"
 
         if color_inv_on_box.collidepoint(mpos):
-            pass
+            color_inversion = "On"
         if color_inv_off_box.collidepoint(mpos):
-            pass
+            color_inversion = "Off"
 
         if eleven_score_box.collidepoint(mpos):
-            pass
+            max_score = 11
         if seven_score_box.collidepoint(mpos):
-            pass
+            max_score = 7
         if fifteen_score_box.collidepoint(mpos):
-            pass
+            max_score = 15
 
         if back_box.collidepoint(mpos):
-            pass
+            options_screen = False
+            start_screen = True
 
     # ... device-polling
     keys = pygame.key.get_pressed()
@@ -167,11 +185,61 @@ while not done:
     elif win_screen:
         win.blit(win_img, (0, 0))
     elif options_screen:
-        win.blit(options_font.render("Options", True, (255, 255, 255)), (275, 15))
-        win.blit(game_font.render("Difficulty:    Easy    Medium    Hard", True, (255, 255, 255)), (10, 150))
-        win.blit(game_font.render("Winning Score:     7      11      15", True, (255, 255, 255)), (10, 280))
-        win.blit(game_font.render("Color Inversion:     Yes      No", True, (255, 255, 255)), (10, 410))
-        win.blit(game_font.render("Back", True, (255, 255, 255)), (350, 520))
+        win.blit(options_font.render("Options", True, WHT), (275, 15))
+        win.blit(game_font.render("Difficulty:", True, WHT), (10, 150))
+        win.blit(game_font.render("Winning Score:", True, WHT), (10, 280))
+        win.blit(game_font.render("Color Inversion:", True, WHT), (10, 410))
+        win.blit(game_font.render("Back", True, WHT), (350, 520))
+
+        if dif == "Easy":
+            win.blit(game_font.render("Easy", True, WHT), (300, 150))
+            win.blit(game_font.render("Medium", True, GREY), (450, 150))
+            win.blit(game_font.render("Hard", True, GREY), (675, 150))
+
+        elif dif == "Medium":
+            win.blit(game_font.render("Easy", True, GREY), (300, 150))
+            win.blit(game_font.render("Medium", True, WHT), (450, 150))
+            win.blit(game_font.render("Hard", True, GREY), (675, 150))
+
+        elif dif == "Hard":
+            win.blit(game_font.render("Easy", True, GREY), (300, 150))
+            win.blit(game_font.render("Medium", True, GREY), (450, 150))
+            win.blit(game_font.render("Hard", True, WHT), (675, 150))
+
+        if max_score == 7:
+            win.blit(game_font.render("7", True, WHT), (400, 280))
+            win.blit(game_font.render("11", True, GREY), (500, 280))
+            win.blit(game_font.render("15", True, GREY), (625, 280))
+
+        elif max_score == 11:
+            win.blit(game_font.render("7", True, GREY), (400, 280))
+            win.blit(game_font.render("11", True, WHT), (500, 280))
+            win.blit(game_font.render("15", True, GREY), (625, 280))
+
+        elif max_score == 15:
+            win.blit(game_font.render("7", True, GREY), (400, 280))
+            win.blit(game_font.render("11", True, GREY), (500, 280))
+            win.blit(game_font.render("15", True, WHT), (625, 280))
+
+        if color_inversion == "On":
+            win.blit(game_font.render("On", True, WHT), (400, 410))
+            win.blit(game_font.render("Off", True, GREY), (550, 410))
+
+        elif color_inversion == "Off":
+            win.blit(game_font.render("On", True, GREY), (400, 410))
+            win.blit(game_font.render("Off", True, WHT), (550, 410))
+
+        # Test
+        pygame.draw.rect(win, WHT, dif_easy_box, 1)
+        pygame.draw.rect(win, WHT, dif_medium_box, 1)
+        pygame.draw.rect(win, WHT, dif_hard_box, 1)
+        pygame.draw.rect(win, WHT, color_inv_on_box, 1)
+        pygame.draw.rect(win, WHT, color_inv_off_box, 1)
+        pygame.draw.rect(win, WHT, eleven_score_box, 1)
+        pygame.draw.rect(win, WHT, seven_score_box, 1)
+        pygame.draw.rect(win, WHT, fifteen_score_box, 1)
+        pygame.draw.rect(win, WHT, back_box, 1)
+
     elif not start_screen:
         if round_start is False:
             win.blit(game_font.render("Space to Start Round", True, (255, 0, 0)), (190, 100))
